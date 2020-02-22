2046165ab90bc3c1e205cc7b71c8b9bf9222a0e8
IsaiahWitzke <isaiah.witzke@gmail.com>
2020-02-21 21:35:53 -0500

    fixed username and password boxes

--- chameleon.js
+++ chameleon.js
@@ -1,11 +1,18 @@
-// will add the FATdn class to the username/password fields
+// will add/remove the FATdn class to the username/password fields
 function onUsernameInput() {
     var inputText = document.getElementById("username-input-box").value;
     if(inputText) {
-        document.getElementById("username-input-label").className = "_9nyy2 FATdn";    
+        document.getElementById("username-input-label").className = "f0n8F FATdn";    
     } else {
-        document.getElementById("username-input-label").className = "_9nyy2";
-        // bruh
+        document.getElementById("username-input-label").className = "f0n8F ";
     }
-    
+}
+
+function onPasswordInput() {
+    var inputText = document.getElementById("password-input-box").value;
+    if(inputText) {
+        document.getElementById("password-input-label").className = "f0n8F FATdn";    
+    } else {
+        document.getElementById("password-input-label").className = "f0n8F ";
+    }
 }
