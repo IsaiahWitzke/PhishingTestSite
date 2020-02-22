592e4ffefa70201217304bd9553bc6b8d862ebe6
IsaiahWitzke <isaiah.witzke@gmail.com>
2020-02-21 21:17:24 -0500

    username box

---
+++ chameleon.js
@@ -0,0 +1,5 @@
+// will add the FATdn class to the username/password fields
+function onUsernameInput() {
+    var input = document.getElementById("username-input-box").value;
+    document.getElementById("username-input-span").className = "_9nyy2 FATdn";
+}
